6486bb6dfde1ffd55e7f7908edde10578ba9794b
FelixPolkanov <felixpolkanov@gmail.com>
2023-01-29 20:30:33 +0200

    add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from "./redux/Store.js";
+import { store, persistor } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
